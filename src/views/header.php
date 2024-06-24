22ba4feaac5b1f2499787cc487e665211e8e11ee
Nicolas Santana <164400737+Santanadev5@users.noreply.github.com>
2024-06-24 20:28:39 -0300

    Add files via upload

---
+++ src/views/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestor de Finanças</title>
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="public/js/scripts.js"></script>
+</head>
+<body>
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">Gestor de Finanças</a>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?action=listar_receitas">Receitas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?action=listar_despesas">Despesas</a>
+            </li>
+        </ul>
+    </div>
+</nav>
