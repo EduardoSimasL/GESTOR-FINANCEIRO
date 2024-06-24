f4df6b8adfd9f1ed5588050a6333555a9b8fdbca
Eduardo Simas <eduardosimas12@gmail.com>
2024-06-24 20:48:46 -0300

    js

--- src/views/header.php
+++ src/views/header.php
@@ -6,6 +6,8 @@
     <title>Gestor de Finanças</title>
     <link rel="stylesheet" href="public/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="public/js/scripts.js"></script>
 </head>
 <body>
