5ea35ffeb61ffc618dd0cdd425fff262b1374081
Nicolas Santana <nicolassantanadev@gmail.com>
2024-06-26 13:30:20 -0300

    header

--- src/views/header.php
+++ src/views/header.php
@@ -5,23 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestor de Finanças</title>
     <link rel="stylesheet" href="public/css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="public/js/scripts.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
-<div class="container">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">Gestor de Finanças</a>
-    <div class="collapse navbar-collapse">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="index.php?action=listar_receitas">Receitas</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php?action=listar_despesas">Despesas</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+
+<header>    
+    <nav>
+      <ul>
+        <a href="#">
+        
+          <li><i class="fa-solid fa-house"></i> Home</li>
+        </a>
+
+        <a href="#">
+          <li><i class="fa-solid fa-sliders"></i> Gráficos</li>
+        </a>
+        <a href="#">
+          <li> <i class="fa-solid fa-list"></i> Relatórios</li>
+        </a>
+      </ul>
+    </nav>
+</header>
