c3ac70861fe1c47faba082e565bd40e67020833e
Eduardo Simas <92694555+EduardoSimasL@users.noreply.github.com>
2024-06-26 23:53:15 -0300

    Add files via upload

--- src/views/header.php
+++ src/views/header.php
@@ -1,32 +1,43 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestor de Finanças</title>
+    <title>Gestor Financeiro</title>
     <link rel="stylesheet" href="public/css/style.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="public/js/scripts.js"></script>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-
-<header>    
-    <nav>
-      <ul>
-        <a href="#">
+<body>
+    
+    <header>
         
-          <li><i class="fa-solid fa-house"></i> Home</li>
-        </a>
+        <nav>
+          <ul>
+            <a href="index.php">
+              <li><i class="fa-solid fa-house"></i> Home</li>
+            </a>
+            <a href="index.php?action=adicionar_receita">
+              <li> <i class="fa-solid fa-list"></i> Adicionar Receita</li>
+            </a>
+            <a href="index.php?action=adicionar_despesa">
+              <li> <i class="fa-solid fa-list"></i> Adicionar Despesa</li>
+            </a>
+            <a href="index.php?action=listar_receitas">
+              <li><i class="fa-solid fa-sliders"></i> Receitas</li>
+            </a>
+            <a href="index.php?action=listar_despesas">
+              <li><i class="fa-solid fa-sliders"></i> Despesas</li>
+            </a>
+            
+          </ul>
+        </nav>
+    </header>
 
-        <a href="#">
-          <li><i class="fa-solid fa-sliders"></i> Gráficos</li>
-        </a>
-        <a href="#">
-          <li> <i class="fa-solid fa-list"></i> Relatórios</li>
-        </a>
-      </ul>
-    </nav>
-</header>
+    <div class="pai">
+        <div class="filho">
+            <h1 class="filhoh1">Gestor de Despesas <i class="fa-solid fa-calendar-days"></i></h1>
+        </div>
+    </div>
--- src/views/home.php
+++ src/views/home.php
@@ -1,25 +1,7 @@
 <?php include 'header.php'; ?>
 <!-- body aqui -->
-<h2>Adicionar Receita</h2>
-<form action="index.php?action=adicionar_receita" method="POST">
-    <div class="form-group">
-        <label for="valor">Valor</label>
-        <input type="number" step="0.01" class="form-control" id="valor" name="valor" required>
-    </div>
-    <div class="form-group">
-        <label for="descricao">Descrição</label>
-        <input type="text" class="form-control" id="descricao" name="descricao">
-    </div>
-    <div class="form-group">
-        <label for="data">Data</label>
-        <input type="date" class="form-control" id="data" name="data" required>
-    </div>
-    <div class="form-group">
-        <label for="categoria">Categoria</label>
-        <input type="text" class="form-control" id="categoria" name="categoria">
-    </div>
-    <button type="submit" class="btn btn-primary">Adicionar</button>
-</form>
+<h2>Home</h2>
+
 <?php include 'footer.php'; ?>
 
 
