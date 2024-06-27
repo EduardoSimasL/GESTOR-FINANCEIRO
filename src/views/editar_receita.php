06da40dac2d1bb03145ab145f2933b31e176782b
Sabrinna <147003671+Ssteixeira@users.noreply.github.com>
2024-06-27 00:44:30 -0300

    Add files via upload

---
+++ src/views/editar_receita.php
@@ -0,0 +1,23 @@
+<?php include 'header.php'; ?>
+<h2>Editar Receita</h2>
+<form action="index.php?action=editar_receita" method="POST">
+    <input type="hidden" name="id" value="<?php echo $receita['id']; ?>">
+    <div class="form-group">
+        <label for="valor">Valor</label>
+        <input type="number" step="0.01" class="form-control" id="valor" name="valor" value="<?php echo $receita['valor']; ?>" required>
+    </div>
+    <div class="form-group">
+        <label for="descricao">Descrição</label>
+        <input type="text" class="form-control" id="descricao" name="descricao" value="<?php echo $receita['descricao']; ?>">
+    </div>
+    <div class="form-group">
+        <label for="data">Data</label>
+        <input type="date" class="form-control" id="data" name="data" value="<?php echo $receita['data']; ?>" required>
+    </div>
+    <div class="form-group">
+        <label for="categoria">Categoria</label>
+        <input type="text" class="form-control" id="categoria" name="categoria" value="<?php echo $receita['categoria']; ?>">
+    </div>
+    <button type="submit" class="btn btn-primary">Salvar</button>
+</form>
+<?php include 'footer.php'; ?>
